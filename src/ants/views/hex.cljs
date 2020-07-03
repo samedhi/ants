afb50bc5b1050f053cc489d9802b4b5e5426a8aa
Stephen Cagle <stephen@read-line.com>
2020-07-03 13:30:56 -0700

    Render a grid of hexes

--- src/ants/views/hex.cljs
+++ src/ants/views/hex.cljs
@@ -4,13 +4,18 @@
    [ants.mui :as mui]))
 
 (defn component []
-  [:div {:id :grid :class :clear}
-   (for [row-i (range 1)]
-     ^{:key row-i}
-     [:div {:class :row}
-      (for [col-i (range 10)]
-        ^{:key (str "row-" row-i ":col-" col-i)}
-        [:div {:class :column}
-         [:li
-          [:div {:class :hexagon}]]])])])
+  (let [row-count 10
+        column-count 19]
+    [:div {:id :grid :class :clear}
+     (for [row-i (range row-count)]
+       ^{:key row-i}
+       [:div {:class :row
+              :style (merge
+                      (when (pos? row-i) {:margin-top (str (/ -100 column-count 7.5) "%")})
+                      (when (odd? row-i) {:right (str (/ 100 column-count 2) "%")}))}
+        (for [col-i (range column-count)]
+          ^{:key (str "row-" row-i ":col-" col-i)}
+          [:div {:class :column}
+           [:li
+            [:div {:class :hexagon}]]])])]))
 
