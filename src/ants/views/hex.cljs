6efec85b5849c9fe72993f09376a5d6ef78c411e
Stephen Cagle <stephen@read-line.com>
2020-07-03 19:13:55 -0700

    Rotate and display image with tiles

--- src/ants/views/hex.cljs
+++ src/ants/views/hex.cljs
@@ -4,25 +4,38 @@
    [ants.mui :as mui]
    [ants.views.images :as images]))
 
-(defn image-portion [image-map row column]
-  (let [{:keys [src width height rows columns]} image-map
-        section-width (/ width columns)
-        section-height (/ height rows)]
-    [:div {:class :img-container}
-     [:img {:src src
-            :style {:position :relative
-                    :top (str "-" row "00%")
-                    :left (str "-" column "00%")
-                    :height (str rows "00%")
-                    :width (str columns "00%")
-                    }}]]))
+(def facing->degrees
+  {:northeast 30
+   :east 90
+   :southeast 150
+   :southwest 210
+   :west 270
+   :northwest 330})
 
-(defn hex [x y]
+(defn image
+  ([image-map]
+   (image image-map :northeast 0 0))
+  ([image-map facing]
+   (image image-map facing 0 0))
+  ([image-map facing row column]
+   (let [{:keys [src width height rows columns] :or {:rows 1 :columns 1}} image-map
+         section-width (/ width columns)
+         section-height (/ height rows)]
+     [:div {:class :img-container
+            :style {:transform (str "rotate(" (facing->degrees facing) "deg)")}}
+      [:img {:src src
+             :style {:position :relative
+                     :top (str "-" row "00%")
+                     :left (str "-" column "00%")
+                     :height (str rows "00%")
+                     :width (str columns "00%")}}]])))
+
+(defn tile [x y]
   (let []
     [:div {:class :column}
      [:li
       [:div {:class :hexagon}
-       [image-portion images/ant-walk x y]]]]))
+       [image images/ant-walk :southwest x y]]]]))
 
 (defn component []
   (let [row-count 5
@@ -36,5 +49,5 @@
                       (when (odd? row-i) {:right (str (/ 100 column-count 2) "%")}))}
         (for [col-i (range column-count)]
           ^{:key (str "row-" row-i ":col-" col-i)}
-          [hex row-i col-i])])]))
+          [tile row-i col-i])])]))
 
