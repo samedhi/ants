c1928aec7272982bd8a85a51c3f28ff7d9c2cd30
Stephen Cagle <stephen@read-line.com>
2020-07-03 18:48:57 -0700

    Render an ant facing NE

--- src/ants/views/hex.cljs
+++ src/ants/views/hex.cljs
@@ -1,11 +1,32 @@
 (ns ants.views.hex
   (:require
    [re-frame.core :as re-frame]
-   [ants.mui :as mui]))
+   [ants.mui :as mui]
+   [ants.views.images :as images]))
+
+(defn image-portion [image-map row column]
+  (let [{:keys [src width height rows columns]} image-map
+        section-width (/ width columns)
+        section-height (/ height rows)]
+    [:div {:class :img-container}
+     [:img {:src src
+            :style {:position :relative
+                    :top (str "-" row "00%")
+                    :left (str "-" column "00%")
+                    :height (str rows "00%")
+                    :width (str columns "00%")
+                    }}]]))
+
+(defn hex [x y]
+  (let []
+    [:div {:class :column}
+     [:li
+      [:div {:class :hexagon}
+       [image-portion images/ant-walk x y]]]]))
 
 (defn component []
-  (let [row-count 10
-        column-count 19]
+  (let [row-count 5
+        column-count 5]
     [:div {:id :grid :class :clear}
      (for [row-i (range row-count)]
        ^{:key row-i}
@@ -15,7 +36,5 @@
                       (when (odd? row-i) {:right (str (/ 100 column-count 2) "%")}))}
         (for [col-i (range column-count)]
           ^{:key (str "row-" row-i ":col-" col-i)}
-          [:div {:class :column}
-           [:li
-            [:div {:class :hexagon}]]])])]))
+          [hex row-i col-i])])]))
 
---
+++ src/ants/views/images.cljs
@@ -0,0 +1,7 @@
+(ns ants.views.images)
+
+(def ant-walk {:src "img/walk.png"
+               :width 1616
+               :height 1984
+               :rows 8
+               :columns 8})
