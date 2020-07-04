2a9c968285f49e49475d980e76f330998c803c66
Stephen Cagle <stephen@read-line.com>
2020-07-04 16:06:36 -0700

    Add config specifying relation between :facing other sytems

--- src/ants/config.cljs
+++ src/ants/config.cljs
@@ -15,6 +15,28 @@
    :west 270
    :northwest 330})
 
+(def facing->potentials
+  {:northeast [:northwest :northeast :east]
+   :east [:northeast :east :southeast]
+   :southeast [:east :southeast :southwest]
+   :southwest [:southeast :southwest :west]
+   :west [:southwest :west :northwest]
+   :northwest [:west :northwest :northeast]})
+
+(def even->facing->coordinate-delta
+  {:even {:northeast [-1 1]
+          :east [0 1]
+          :southeast [1 1]
+          :southwest [1 0]
+          :west [0 -1]
+          :northwest [-1 0]}
+   :odd {:northeast [-1 0]
+         :east [0 1]
+         :southeast [1 0]
+         :southwest [1 -1]
+         :west [0 -1]
+         :northwest [-1 -1]}})
+
 (def default-db {:row-count 3
                  :column-count 5
                  :ants {[2 2] {:facing :northeast}}})
